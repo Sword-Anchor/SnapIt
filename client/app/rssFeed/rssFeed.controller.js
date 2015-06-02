1447076b716fac29ac513c8c8428af745cb8df39
Siddharth Sukumar <sidreddy88@gmail.com>
2015-06-01 17:47:15 -0700

    Cleaned up code and added a new property rssUrls to the user schema

--- client/app/rssFeed/rssFeed.controller.js
+++ client/app/rssFeed/rssFeed.controller.js
@@ -1,37 +1,40 @@
 'use strict';
 
 angular.module('snapItApp')
-  .controller("FeedCtrl", ['$scope','FeedService', 'BlogList', function ($scope,FeedService,BlogList){
+  .controller("RssCtrl", ['$scope','FeedService', 'FeedList', '$http', 
+    function ($scope,FeedService,FeedList,$http){
     $scope.feedUrls = [];
     $scope.ObjectUrls = {};
 
-    $scope.jsonConcat = function(o1, o2) {
-       for (var key in o2) {
-        o1[key] = o2[key];
-       }
-       return o1;
-    }
+    /*
+     when user clicks on the loadUrl button, the users urls are fetched, and the enw url is
+     compared to the urls returned from the server. If there is a new url, then it is 
+     passed to updateFeed so that a list of feed entries can be populated in the database
+    */
 
-    debugger;
+   
+
     $scope.addUrls = function(url) {
       $(".feeds").empty();
-      var found = false;
-      for (var i = 0; i < $scope.feedUrls.length; i++) {
-        if (url === $scope.feedUrls[i]){
-          found = true;
-      }
-    }
-    if (!found){
-      $scope.feedUrls.push(url);
-     }
-    };
+      $http.get('/api/users/me').success(function(user) {
+          debugger;
+          $scope.returnedUrls = user.rssUrls;
+          var found = false;
+            for (var i = 0; i < $scope.returnedUrls.length; i++) {
+              if (url === $scope.returnedUrls[i]){
+                found = true;
+               }
+              }
+         if (!found){
+           $scope.feedUrls.push(url);
+         }
+         $scope.updateFeed();
+      });
+      };
 
     $scope.updateFeed =  function (){
-    debugger;
-      BlogList.get($scope.feedUrls)
+      FeedList.get($scope.feedUrls)
         .then (function (data) {
-        
-          //var object = $scope.jsonConcat($scope.ObjectUrls, data);
           debugger;
           $scope.feeds = data;
         });
@@ -44,17 +47,17 @@ angular.module('snapItApp')
         parseFeed: function (url) { 
            return $http.jsonp('https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=1&q=' + url +
            '&callback=JSON_CALLBACK');
-    }
-  }
+         }
+      }
 
   }])
 
-  .service('FeedList', ['$rootScope', 'FeedService', '$q', 
-    function ($rootScope, FeedService, $q) {
+  .service('FeedList', ['$rootScope', 'FeedService', '$q', function ($rootScope, FeedService, $q) {
     var feedVar;
     var feeds = [];
     var deferred = $q.defer();
     console.log("loading feeds...");
+
     this.get = function(urls) {
     //var feeds = [];
     for (var i=0; i<urls.length; i++) {
@@ -66,10 +69,8 @@ angular.module('snapItApp')
           feeds.push(feedVar);
           console.log(feedVar);
           deferred.resolve(feeds);    
-        })       
-                
+        })                    
       }
-
         debugger;
         return deferred.promise;
     };
