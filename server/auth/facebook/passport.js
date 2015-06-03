325c78b1ce1b567d37f9018513abde62447843de
Vini <gvinaya@hotmail.com>
2015-06-03 15:44:34 -0700

    s3Upload

--- server/auth/facebook/passport.js
+++ server/auth/facebook/passport.js
@@ -2,6 +2,8 @@ var passport = require('passport');
 var FacebookStrategy = require('passport-facebook').Strategy;
 
 exports.setup = function (User, config) {
+
+
   passport.use(new FacebookStrategy({
       clientID: config.facebook.clientID,
       clientSecret: config.facebook.clientSecret,
--- server/auth/local/index.js
+++ server/auth/local/index.js
@@ -7,7 +7,10 @@ var auth = require('../auth.service');
 var router = express.Router();
 
 router.post('/', function(req, res, next) {
+  console.log("in post");
   passport.authenticate('local', function (err, user, info) {
+    console.log("in local authenticate");
+
     var error = err || info;
     if (error) return res.json(401, error);
     if (!user) return res.json(404, {message: 'Something went wrong, please try again.'});
