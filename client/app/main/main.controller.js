e720da5b227d019d173be95057d192b5abf00222
Siddharth Sukumar <sidreddy88@gmail.com>
2015-06-03 12:56:51 -0700

    Modified front end code to show articles from rss feeds too

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -11,7 +11,9 @@ angular.module('snapItApp')
     $scope.searchTxt = '';
     $scope.searchDate = true;
     $scope.isCollapsed = false;
+
     $scope.userEmail = Auth.getUserEmail();
+
 
     $http.get('/api/things/getItems/?email='+ $scope.userEmail).success(function(pics) {
       $scope.pics = pics;
@@ -40,7 +42,7 @@ angular.module('snapItApp')
     };
 
     $scope.isSelection = function(mediaType) {
-      if (mediaType === 'selection') {
+      if (mediaType === 'selection' || mediaType === 'rssFeed') {
         return true;
       }
       return false;
@@ -55,6 +57,13 @@ angular.module('snapItApp')
 
     $scope.isLink = function(mediaType) {
       if (mediaType === 'links') {
+        return true;
+      }
+      return false;
+    };
+
+    $scope.isRssFeed = function(mediaType) {
+      if (mediaType === 'rssFeed') {
         return true;
       }
       return false;
