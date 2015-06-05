ca89e7d4c8668616083cc1f6aab870a3a2126bc2
Vince <nocera.vincent@gmail.com>
2015-06-04 17:14:56 -0700

    invite backend

---
+++ client/app/invite/invite.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('snapItApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('invite', {
+        url: '/invite',
+        templateUrl: 'app/invite/invite.html',
+        controller: 'InviteCtrl'
+      })
+  });
