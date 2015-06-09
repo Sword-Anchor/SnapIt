903fce9060bf96cd52edd0e9e502901fc80059c1
Jennifer Bland <ratracegrad@gmail.com>
2015-06-09 11:24:21 -0400

    Removed create folder in app since it is not being used. Removed as part of code cleanup

--- client/app/create/create.controller.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('snapItApp')
-  .controller('CreateCtrl', ['$scope', function ($scope) {
-    $scope.title='create';
-    console.log('*** create controller ***');
-  }]);
